b93250d75cb4df02072eb90a279c1030e5a6c85a
nanda <nandakumaroye@gmail.com>
2024-08-18 15:54:31 +0530

    added databaseloader.py file and its associated files

---
+++ app-file-format-convertor-db-file-loader/hw.py
@@ -0,0 +1,11 @@
+import sys
+import os
+args = sys.argv
+arg = args[1]
+# prog_file = args[2]
+
+host = os.environ.get('HOST')
+
+
+# print(f"Hello {arg} from {prog_file}")
+# print(f"Connecting to {host}")
